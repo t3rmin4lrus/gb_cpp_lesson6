a5f2fc7b6f993bf186fdab992c8df042d17c58de
Vladislav Galimyanov <dvhrug@yandex.ru>
2021-08-10 00:48:16 +0500

    Task 2: dynamic allocation of a matrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 void init_array(uint* array, size_t array_size);
 void print_array(uint* array, size_t array_size);
+void init_matrix(int** matrix, size_t const rows, size_t const cols);
+void print_matrix(int** matrix, size_t const rows, size_t const cols);
 
 int main()
 {
@@ -31,6 +35,25 @@ int main()
             return 1;
         }
     } // Task 1
+
+    // Task 2
+    {
+        int** matrix;
+        size_t const M = 4;
+        size_t const N = 4;
+
+        matrix = new int*[M];
+        for (size_t i = 0; i < M; i++)
+            matrix[i] = new int[N];
+
+        init_matrix(matrix, M, N);
+        print_matrix(matrix, M, N);
+
+        for (size_t i = 0; i < M; i++)
+            delete[] matrix[i];
+        delete[] matrix;
+        matrix = nullptr;
+    } // Task 2
 }
 
 void init_array(uint* array, size_t array_size)
@@ -46,4 +69,25 @@ void print_array(uint* array, size_t array_size)
     for (size_t i = 0; i < array_size; i++)
         std::cout << array[i] << " ";
     std::cout << '\n';
+}
+
+void init_matrix(int** matrix, size_t const rows, size_t cols)
+{
+    std::srand(std::time(nullptr));
+
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            matrix[i][j] = std::rand();
+        }
+    }
+}
+
+void print_matrix(int** matrix, size_t const rows, size_t cols)
+{
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << '\n';
+    }
 }
